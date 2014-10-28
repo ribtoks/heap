78ea7d57771cf60fe919095677f43dbf47038575
Taras Kushnir <kushnirTV@gmail.com>
2014-10-28 18:09:03 +0200

    Added one of the last slides and improved several previous

--- GoLangSlides/materials/constants_demo.go
+++ GoLangSlides/materials/constants_demo.go
@@ -4,7 +4,7 @@ func main() {
 
   // START OMIT
   const Ln2 = 0.693147180559945309417232121458176568075500134360255254120680009
-  const Log2E= 1/Ln2
+  const Log2E = 1/Ln2
   var x float64 = Log2E
 
   println(x)
