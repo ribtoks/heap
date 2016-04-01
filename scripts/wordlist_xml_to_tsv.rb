c460d5d208d593d99d7121ba0530c7a144f75d9b
Taras Kushnir <takushni@microsoft.com>
2016-04-01 14:38:52 +0300

    Skipping accents and non-ascii symbols

--- scripts/wordlist_xml_to_tsv.rb
+++ scripts/wordlist_xml_to_tsv.rb
@@ -1,5 +1,12 @@
 require 'nokogiri'
 
+def is_bad_word?(word)
+  any_non_ascii = word.bytes.any? { |b| b > 127 }
+  #any_tick = word.chars.any? { |c| c == '\'' }
+
+  any_non_ascii #|| !any_tick
+end
+
 doc = File.open(ARGV.first) { |f| Nokogiri::XML(f) }
 
 result_filename = ARGV.first.sub(/[.]xml$/, '.tsv')
@@ -9,7 +16,7 @@ doc.xpath('//w').each do |node|
   frequency = node['f'];
   word = node.content
 
-  tsv_file.puts("#{frequency}	#{word}")
+  tsv_file.puts("#{frequency}\t#{word}") unless is_bad_word? word
 end
 
 tsv_file.close
