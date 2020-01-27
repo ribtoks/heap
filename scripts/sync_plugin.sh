f9b5ed467e748939b67fdf5b3782aecc6a68bbe3
Taras Kushnir <tk.dev@mailbox.org>
2020-01-26 17:38:41 -0800

    Add rclone BitBar plugin

---
+++ scripts/sync_plugin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# <bitbar.title>Sync notebooks</bitbar.title>
+# <bitbar.version>v0.1</bitbar.version>
+# <bitbar.author>Taras Kushnir</bitbar.author>
+# <bitbar.author.github>ribtoks</bitbar.author.github>
+# <bitbar.desc>This will start rclone sync</bitbar.desc>
+# <bitbar.image></bitbar.image>
+# <bitbar.dependencies></bitbar.dependencies>
+
+echo "rclone"
+echo "---"
+
+REMOTE_NAME="Dropbox"
+REMOTE_PATH="MyDropboxCrypt:/"
+LOCAL_PATH="~/CloudSync/"
+
+echo "↑ Sync To ${REMOTE_NAME} | terminal=true bash=\"rclone sync ${LOCAL_PATH} ${REMOTE_PATH} -P\""
+echo "↓ Sync From ${REMOTE_NAME} | terminal=true bash=\"rclone sync ${REMOTE_PATH} ${LOCAL_PATH} -P\""
+
